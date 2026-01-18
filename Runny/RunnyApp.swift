469c8872f41da30ccf28d98b137aa20f0002e836
delightfulll <w0nderfulvinay@gmail.com>
2026-01-18 14:40:13 -0500

    Initial Commit

---
+++ Runny/RunnyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunnyApp.swift
+//  Runny
+//
+//  Created by Vinay Honne  on 1/18/26.
+//
+
+import SwiftUI
+
+@main
+struct RunnyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RunnyTests/RunnyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RunnyTests.swift
+//  RunnyTests
+//
+//  Created by Vinay Honne  on 1/18/26.
+//
+
+import Testing
+@testable import Runny
+
+struct RunnyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
